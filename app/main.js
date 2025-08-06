4622ee4eedc246dcf5b73209d8154bda379fde6d
adityarp0 <aditya290405@gmail.com>
2025-08-06 21:47:54 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -6,7 +7,7 @@ function createWindow() {
     height: 600,
   });
 
-  win.loadFile('LoginForm.html');
+  win.loadFile(path.join(__dirname, 'loginform', 'LoginForm.html'));
 }
 
 app.whenReady().then(createWindow);
