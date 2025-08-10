a9ce598efd99e1957223aff8bb89a89b8c20199f
hypernova2099 <chriseledward@gmail.com>
2025-08-10 12:43:58 +0530

    switched fetch to axios

--- app/index.js
+++ app/index.js
@@ -1,25 +1,34 @@
+import axios from "axios";
+
 document.getElementById('login-form').addEventListener('submit',async(e)=>{
 
     e.preventDefault(); //prevents form from reloading
 
-    const res = await fetch('http://localhost:8080/login',{
-        method : "POST",
-        headers : {
-            "Content-Type" : 'application/json'
-        },
-        body : JSON.stringify({
-            username: document.getElementById('username').value,
-            password: document.getElementById('password').value
-        })
-    });
+    try {
+        const res = await axios.post('http://localhost:8080/login',{
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        });
+        if(res.data.success){
+            window.location.href = "dashboard.html";
+        }
+        else{
+            alert("Login failed")
+        }
 
-    const data = await res.json();
-    console.log(data);
+    
+    } catch (error) {
+        console.error("Login Failed:",error);
+        alert("An error occured , Please Try again later!");
+    }
 
-    if(data.success){
-        window.location.href = "dashboard.html";
-    }
-    else{
-        alert("Login failed")
-    }
+    
+    
 });
+
+
+
+
+
+
+    
